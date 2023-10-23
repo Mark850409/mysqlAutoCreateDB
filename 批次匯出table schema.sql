c48783e61f7ad446f17d1d9ebf5f80529c9e53a3
markhsu <xiaoyan850409@gmail.com>
2023-10-23 10:07:02 +0800

    更新SP，增加描述

--- 批次匯出table schema.sql
+++ 批次匯出table schema.sql
@@ -1,12 +1,29 @@
-DELIMITER $$                                                                                                                                                                    
-DROP PROCEDURE IF EXISTS procCountAllTables $$                                                                                                                                  
+-- ====================================================================
+-- SP Name     : procExportTableSchema
+-- Function    : 批次匯出table schema
+-- Author      : Mark
+-- Create Date : 2023-10-22
+-- Parameter   : DBName
+-- Modify His  : 
+-- (1) 2023-10-22 : Mark    : CREATE 
+-- (2) 2023-10-23 : Mark    : Modify SP describe 
+-- ===================================================================
+
+DELIMITER $$
+-- 如果PROCEDURE存在就先移除                                                                                                                                                                   
+DROP PROCEDURE IF EXISTS procExportTableSchema $$       
+-- 建立PROCEDURE(傳入資料庫名稱參數)                                                                                                                           
 CREATE PROCEDURE procCountAllTables(DBName varchar(255))    
-BEGIN                                                                                                                                                                       
+BEGIN   
+		-- 變數宣告
+        -- 1. 資料表集合名稱
+		-- 2. 取得最後一張資料表
+        -- 3. 時間戳記
         DECLARE table_name VARCHAR(255);                                                                                                                                        
         DECLARE end_of_tables INT DEFAULT 0;
         DECLARE mylocaltimestamp DATETIME;
 		
-        
+        -- 利用SQL取出指定資料庫裡面所有資料表的list，直到最後一張資料表為止
         DECLARE cur CURSOR FOR                                                                                                                                                  
             SELECT t.table_name                                                                                                                                                 
             FROM information_schema.tables t                                                                                                                                    
@@ -15,20 +32,25 @@ BEGIN
 		
         OPEN cur;                                                                                                                                                               
 		
-        
+        -- 迴圈開始
         tables_loop: LOOP                   
             	
+			-- 將資料集透過迴圈取出，最後一張表時強制離開迴圈
             FETCH cur INTO table_name;             			
             IF end_of_tables = 1 THEN                                                                                                                                           
                 LEAVE tables_loop;                                                                                                                                              
             END IF;                                                                                                                                                             
-
-            SET @show_schema = CONCAT("desc"," ",DBName,".",table_name);                                                                               
+			
+            -- 使用CONCAT串接SQL語法
+            SET @show_schema = CONCAT("desc"," ",DBName,".",table_name);
+            -- 建立PREPARE STATEMENT，並執行
             PREPARE stmt FROM @show_schema;                                                                                                                                               
             EXECUTE stmt;       
-            
+            -- 取得當下時間戳記
 			SET @mylocaltimestamp=DATE_FORMAT(NOW(),'%Y-%m-%d %H-%i-%s');
+            -- 取得CSV匯出路徑
             SET @windows_path='C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/';
+            -- 建立匯出CSV的SQL語法
 			SET @export_csv = CONCAT("SELECT 'COLUMN_NAME','DATA_TYPE','IS_NULLABLE','COLUMN_KEY'
 			UNION
 			select COLUMN_NAME,DATA_TYPE,IS_NULLABLE,COLUMN_KEY FROM information_schema.columns 
@@ -37,11 +59,12 @@ BEGIN
 
 			PREPARE stmt2 FROM @export_csv;                                                                                                                                               
 			EXECUTE stmt2; 
-        
+         -- 迴圈結束
         END LOOP;                                                                                                                                                               
         CLOSE cur;                                                                                                                                                              
     END $$                                                                                                                                                                      
-
 DELIMITER ;
+-- 設定資料庫名稱參數 
 SET @DBName='stock';
+-- 呼叫PROCEDURE並傳入參數(資料庫名稱)
 call procCountAllTables(@DBName);
---
+++ 批次建立stock table.sql
@@ -0,0 +1,152 @@
+-- ====================================================================
+-- SP Name     : procExportTableSchema
+-- Function    : 批次建立 stock table
+-- Author      : Mark
+-- Create Date : 2023-10-22
+-- Parameter   : DBName
+-- Modify His  : 
+-- (1) 2023-10-22 : Mark    : CREATE 
+-- (2) 2023-10-23 : Mark    : Modify SP describe 
+-- ===================================================================
+
+-- 如果stock資料庫不存在就建立一個新的
+CREATE DATABASE IF NOT EXISTS stock DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+-- 使用stock資料庫
+USE stock;
+-- 如果PROCEDURE存在就先移除
+DROP PROCEDURE IF EXISTS stock.CreateStockTable; 
+DELIMITER //
+-- 建立PROCEDURE
+CREATE PROCEDURE CreateStockTable()
+BEGIN
+		-- 這邊可以開始寫SQL，以下為建立資料表語法
+		DROP TABLE IF EXISTS stock_daily;
+		CREATE TABLE IF NOT EXISTS stock_daily
+		(
+			sdate varchar(10),
+			id varchar(10),
+			sname varchar(255),
+			trs_qty decimal(18, 0),
+			trs_count decimal(18, 0),
+			trs_amt decimal(18, 0),
+			open_price decimal(18, 2),
+			h_price decimal(18, 2),
+			l_price decimal(18, 2),
+			close_price decimal(18, 2),
+			s_status varchar(255),
+			dec_price decimal(18, 2),
+			lb_price decimal(18, 2),
+			lb_amt decimal(18, 2),
+			ls_price decimal(18, 2),
+			ls_amt decimal(18, 2),
+			PE decimal(18, 2)
+		);
+        
+		DROP TABLE IF EXISTS stock_list;
+		CREATE TABLE IF NOT EXISTS stock_list
+		(
+			id varchar(10),
+			sname varchar(255),
+			stock_attr varchar(255),
+			industry varchar(255),
+			CONSTRAINT PK_stock_list
+				PRIMARY KEY (id)
+		);
+
+		DROP TABLE IF EXISTS stock_monthly_revenue;
+		CREATE TABLE IF NOT EXISTS stock_monthly_revenue
+		(
+			yyyymm varchar(6),
+			id varchar(10),
+			sname varchar(255),
+			month_amt decimal(18, 0),
+			pre_m_amt decimal(18, 0),
+			pre_y_amt decimal(18, 0),
+			pre_m_percent decimal(18, 2),
+			pre_y_percent decimal(18, 2),
+			sum_amt decimal(18, 0),
+			per_sum_amt decimal(18, 0),
+			sum_percent decimal(18, 2),
+			memo_str varchar(255),
+			CONSTRAINT PK_stock_monthly_revenue
+				PRIMARY KEY
+				(
+					id,
+					yyyymm
+				)
+		);
+
+		DROP TABLE IF EXISTS stock_daily_3;
+		CREATE TABLE IF NOT EXISTS stock_daily_3
+		(
+			sdate varchar(10),
+			id varchar(10),
+			f_qty decimal(18, 0),
+			i_qty decimal(18, 0),
+			s_qty decimal(18, 0),
+			t_qty decimal(18, 0),
+			est_f_qty decimal(18, 0),
+			est_i_qty decimal(18, 0),
+			est_s_qty decimal(18, 0),
+			est_t_qty decimal(18, 0),
+			trs_f_percent decimal(18, 2),
+			trs_3_percent decimal(18, 2),
+			CONSTRAINT PK_stock_daily_3
+				PRIMARY KEY
+				(
+					id,
+					sdate
+				)
+		);
+
+		DROP TABLE IF EXISTS stock_daily_MT;
+		CREATE TABLE IF NOT EXISTS stock_daily_MT
+		(
+			sdate varchar(10),
+			id varchar(10),
+			mt_b_qty decimal(18, 0),
+			mt_s_qty decimal(18, 0),
+			mt_r_qty decimal(18, 0),
+			mt_qty decimal(18, 0),
+			mt_dec_qty decimal(18, 0),
+			mt_limit_qty decimal(18, 0),
+			mt_use_percent decimal(18, 2),
+			ss_b_qty decimal(18, 0),
+			ss_s_qt decimal(18, 0),
+			ss_r_qty decimal(18, 0),
+			ss_qty decimal(18, 0),
+			ss_dec_qty decimal(18, 0),
+			ms_percent decimal(18, 2),
+			ms_dec_qty decimal(18, 0),
+			CONSTRAINT PK_stock_daily_MT
+				PRIMARY KEY
+				(
+					id,
+					sdate
+				)
+		);
+
+		DROP TABLE IF EXISTS stock_daily_MR;
+		CREATE TABLE IF NOT EXISTS stock_daily_MR
+		(
+			sdate varchar(10),
+			id varchar(50),
+			id_sub varchar(50),
+			id_mr varchar(50),
+			mrname varchar(255),
+			b_qty decimal(18, 0),
+			s_qty decimal(18, 0),
+			dec_qty decimal(18, 0),
+			dstatus int,
+			trs_percent decimal(18, 2),
+			CONSTRAINT PK_stock_daily_MR
+				PRIMARY KEY
+				(
+					id,
+					sdate
+				)
+		);
+END //
+DELIMITER ;
+-- 呼叫PROCEDURE
+CALL CreateStockTable();
