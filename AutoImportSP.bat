37143ea77c460cb27e289586826dcecd16adce60
markhsu <xiaoyan850409@gmail.com>
2023-10-26 08:55:46 +0800

    fixed: 1.增加MYSQL防呆機制(帳密錯誤、MYSQL服務沒啟動)

--- AutoImportSP.bat
+++ AutoImportSP.bat
@@ -11,13 +11,22 @@ echo.
 echo =========注意事項==========
 echo 1.提示訊息的輸入不得為空!!!
 echo 2.請事先下載好SQL檔案
+echo 3.MYSQL SERVER是否啟動!!!
 echo ===========================
 
 echo.
 
+:CheckMysql
+echo =========以下為您的MYSQL狀態==========
+netstat -ano | findstr 3306
+echo ======================================
+if %ERRORLEVEL% equ 0 goto UserNamePrompt
+echo "MYSQL未啟動，請檢查詳細原因!!!"
+goto end
 
 
 :UserNamePrompt
+echo.
 set /P name=請輸入資料庫帳號：
 if not defined name goto UserNamePrompt
 if %name% neq "" goto pwPrompt
@@ -44,8 +53,12 @@ REM )
 
 :importsql
 "C:/Program Files/MySQL/MySQL Server 8.0/bin/mysql.exe" mysql -u %name% -p%pw% -e "source Createstocktable.sql;" -e "use stock;" -e "source ExportstockTable.sql;"
-goto openfiles
-
+if "%ERRORLEVEL%" EQU "0" (
+  goto openfiles
+) else (
+   echo "MYSQL帳密輸入錯誤，請重新輸入!!!"
+   goto UserNamePrompt
+)
 
 :openfiles
 start  "" "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads"
